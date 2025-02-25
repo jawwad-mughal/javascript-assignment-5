4fe60087112dcb861d28b87878c1612c7b2857fc
unknown <jawad21ullah@gmail.com>
2025-02-25 23:57:24 +0500

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+// ARRAYS AND LOOP (start)
+// Q1
+// var multiArray = [];  
+// console.log('Multidimensional array: ' + multiArray);
+
+// Q2
+// let userinput = +prompt('Enter Number');
+// let range = +prompt('Number range');
+// for(let i = userinput; i <= range; i++){
+//     document.write(i, '<br>');
+// }
+
+// Q3
+// let Table = +prompt("Enter the number for which you want the multiplication table:");
+// let tableRange = +prompt("Enter the length of the multiplication table:");
+// for(let i = 1; i <= tableRange; i++){
+//     document.write(Table, 'X' , i , '=',(Table*i),'<br>');
+// }
