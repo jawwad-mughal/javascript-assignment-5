acd21dd63573f63019eac203398b5a44d943a949
unknown <jawad21ullah@gmail.com>
2025-02-27 01:09:04 +0500

    update

--- app.js
+++ app.js
@@ -16,3 +16,52 @@
 // for(let i = 1; i <= tableRange; i++){
 //     document.write(Table, 'X' , i , '=',(Table*i),'<br>');
 // }
+
+// Q4
+// let fruits = ['apple','banana','mango','orange','strawberry'];
+// for(let i = 0; i < fruits.length; i++){
+//     document.write(fruits[i], '<br>');
+// };
+
+// Q5
+// A)
+var number = [];
+for(let i = 0; i <= 15; i++){
+    number.push(i);
+}
+console.log(number);
+
+// B)
+var number = [];
+for(let i = 10; i >= 1; i--){
+    number.push(i);
+}
+console.log(number);
+
+// C)
+// var num = prompt('Enter Number');
+// var range = prompt('Enter Number range');
+// var number = [];
+// for(let i = num; i <= range; i++){
+//     number.push(i % 2==0);
+// }
+// console.log(number);
+
+// D)
+var num = parseInt(prompt('Enter Starting Number')); // Starting number  
+var range = parseInt(prompt('Enter End Range')); // Ending number 
+
+var even = []
+var odd =[]
+
+// Loop from the starting number to the ending number  
+for (let i = num; i <= range; i++) {  
+    if (i % 2 === 0) {  
+        even.push(i); // If the number is even  
+    } else if (i % 2 !== 0) {  
+        odd.push(i);
+        // console.log(i + " is Odd"); // If the number is odd  
+    }  
+}
+document.write(even  + 'is Even number ');
+document.write(odd + 'is Odd number');
